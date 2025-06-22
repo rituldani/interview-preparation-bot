0dc09225d718ac0089ca7bee88449ade13508bdf
Ritul Dani <rituldani2003@gmail.com>
2025-06-22 16:28:15 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,4 +8,8 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  server: {
+    host: '0.0.0.0', // 👈 expose to external
+    port: process.env.PORT || 5173, // 👈 allow Render to assign the port
+  },
 })
